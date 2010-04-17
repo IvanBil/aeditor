0157b321e3b44854bcbf0b130189a72314c03adb
deimos1986_cp <SND\deimos1986_cp>
2010-04-17 20:24:38 +0000

    "About" message box added with message translated to English and Russian

---
+++ AEditor/HLinkMessageBox.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace AEditor
+{
+    partial class HLinkMessageBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbMessage = new System.Windows.Forms.RichTextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbMessage
+            // 
+            this.rtbMessage.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.rtbMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbMessage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rtbMessage.Location = new System.Drawing.Point(0, 0);
+            this.rtbMessage.Name = "rtbMessage";
+            this.rtbMessage.ReadOnly = true;
+            this.rtbMessage.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.rtbMessage.Size = new System.Drawing.Size(284, 215);
+            this.rtbMessage.TabIndex = 0;
+            this.rtbMessage.Text = "";
+            this.rtbMessage.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtbMessage_LinkClicked);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(105, 227);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // HLinkMessageBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.rtbMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "HLinkMessageBox";
+            this.Load += new System.EventHandler(this.fAbout_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbMessage;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ AEditor/HLinkMessageBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace AEditor
+{
+    public partial class HLinkMessageBox : Form
+    {
+        public HLinkMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        public HLinkMessageBox(string Text, string Header):this()
+        {
+            this.Text = Header;
+            this.AutoSize = true;
+            this.rtbMessage.AutoSize = true;
+            this.rtbMessage.Text = Text;
+            //this.rtbMessage.Height
+        }
+
+        public static void Show(string Text, string Header)
+        {
+            HLinkMessageBox msg = new HLinkMessageBox(Text, Header);
+            msg.Show();
+        }
+
+        private void fAbout_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rtbMessage_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            try
+            {
+                Process.Start(e.LinkText);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
